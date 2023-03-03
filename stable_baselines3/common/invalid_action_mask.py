994eeb9c2e373a782b4923cb25ed25b96bc420ac
Jett Wang <jett@mit.edu>
2023-03-03 07:59:25 -0500

    assume infos already contains tensor masks

--- stable_baselines3/common/invalid_action_mask.py
+++ stable_baselines3/common/invalid_action_mask.py
@@ -6,8 +6,10 @@ def get_mask_from_infos(infos: Optional[Dict], action_space: spaces.Space, devic
     if infos is None:
         return None
     else:
-        return th.stack([
-            th.Tensor(
-                info.get("invalid_action_mask", th.ones(action_space.shape))
-            ) for info in infos
-        ]).to(dtype=th.bool, device=device)
+        masks = []
+        for info in infos:
+            if "invalid_action_mask" in info:
+                masks.append(info.get("invalid_action_mask"))
+            else:
+                masks.append(th.ones(action_space.shape, dtype=th.bool, device=device))
+        return th.stack(masks)
